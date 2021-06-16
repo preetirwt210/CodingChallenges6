53c8f86ac60f5bb678f847400b891d6d956db92a
Preeti Rawat <preetirawat711@gmail.com>
2021-06-16 13:05:42 +0530

    BubbleSort Algo

--- src/academy/learnprogramming/BubbleSort.java
+++ src/academy/learnprogramming/BubbleSort.java
@@ -2,6 +2,20 @@ package academy.learnprogramming;
 
 public class BubbleSort {
 
+    public static void bubbleSort(int[] intArray){
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if(intArray[i]> intArray[i+1]){
+                    BubbleSort.swap(intArray, i, i + 1);
+                }
+
+            }
+
+        }
+        for(int i=0; i<intArray.length;i++){
+            System.out.println(intArray[i]);
+        }
+    }
     public static void swap(int[] array, int i, int j){
         if(i==j){
             return;
